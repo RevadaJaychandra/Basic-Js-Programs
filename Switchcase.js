a25a091450f1f1f13729525a379ba75522d20607
RevadaJaychandra <jayachandra.k1621@gmail.com>
2026-05-04 14:19:14 +0530

    basic programs

---
+++ Switchcase.js
@@ -0,0 +1,19 @@
+let n1=5;
+let n2=10;
+let ch='+';
+switch(ch){
+    case '+':
+        console.log(n1+n2);
+        break;
+    case '-':
+        console.log(n1-n2);
+        break;
+    case '*':
+        console.log(n1*n2);
+        break;
+    case '/':
+        console.log(n1/n2);
+        break;
+    default:
+        console.log("Invalid operator");
+}
